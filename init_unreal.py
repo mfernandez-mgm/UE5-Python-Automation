fc5dfda51124500e01b3f8645642123f463d5660
Mark Fernandez <572243@mgmresorts.com>
2023-04-19 10:12:47 -0700

    added init python file, initilized python classes on engine start.

---
+++ init_unreal.py
@@ -0,0 +1,8 @@
+# Copyright Epic Games, Inc. All Rights Reserved.
+
+# Custom MoviePipelinePythonHostExecutor
+import CustomMoviePipelineExecutor
+# Python wrapper for importing Omniverse Facial Animations
+from unreal import OmniverseFacialAnimationWrapper
+# Python server, used to receive requests from the web app
+import server
